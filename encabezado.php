12f8e40e7bfa1ff611ed14bc4f4cafc5bd26d899
pedro2806 <isc.pedro.mtz@gmail.com>
2025-08-26 12:24:20 -0600

    Update encabezado.php

--- encabezado.php
+++ encabezado.php
@@ -34,29 +34,6 @@
     <!-- Topbar Navbar -->
     <ul class="navbar-nav ml-auto">
 
-        <!-- Nav Item - Search Dropdown (Visible Only XS) -->
-        <li class="nav-item dropdown no-arrow d-sm-none">
-            <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button"
-                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <i class="fas fa-search fa-fw"></i>
-            </a>
-            <!-- Dropdown - Messages -->
-            <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in"
-                aria-labelledby="searchDropdown">
-                <form class="form-inline mr-auto w-100 navbar-search">
-                    <div class="input-group">
-                        <input type="text" class="form-control bg-light border-0 small"
-                            placeholder="Search for..." aria-label="Search"
-                            aria-describedby="basic-addon2">
-                        <div class="input-group-append">
-                            <button class="btn btn-primary" type="button">
-                                <i class="fas fa-search fa-sm"></i>
-                            </button>
-                        </div>
-                    </div>
-                </form>
-            </div>
-        </li>
         <div class="topbar-divider d-none d-sm-block"></div>
         <!-- Nav Item - User Information -->
         <li class="nav-item dropdown no-arrow">
@@ -71,12 +48,12 @@
                 if (strpos($currentURL, "/incidencias/SalasDeJuntas/") !== false || 
                     strpos($currentURL, "/incidencias/inicio") !== false) {
                     echo '<img class="img-profile rounded-circle" 
-                         src="/incidencias/img/undraw_profile.svg" 
-                         style="width: 100%;">';
+                        src="/incidencias/img/undraw_profile.svg" 
+                        tyle="width: 100%;">';
                 } else {
                     echo '<img class="img-profile rounded-circle" 
-                         src="/incidencias/img/undraw_profile.svg"  
-                         style="width: 100%;">';
+                        src="/incidencias/img/undraw_profile.svg"  
+                        style="width: 100%;">';
                 }
                 ?>
             </a>
