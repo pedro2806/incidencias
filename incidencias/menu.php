a9ec3dc904f98b1d23c3318c664d3c5842346346
pedro2806 <isc.pedro.mtz@gmail.com>
2025-09-22 08:20:41 -0600

    version 1
    version 1

--- incidencias/menu.php
+++ incidencias/menu.php
@@ -1,7 +1,7 @@
 <?php
     include '../conn.php';
     if($_COOKIE['noEmpleado'] == '' || $_COOKIE['noEmpleado'] == null){
-        echo '<script>window.location.assign("index")</script>';
+        echo '<script>window.location.assign("../index")</script>';
     }
 ?>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -30,14 +30,24 @@
     <span class="badge text-xl-white">Opciones</span>
 </div>
 <!-- Nav Item - Pages Collapse Menu -->
-<li class="nav-item">
-    <a class="nav-link" href="../inicio">
+<?php
+$usuariosRegistran = array(212, 14, 42, 161, 403, 183, 521, 276);
+
+if (in_array($_COOKIE['noEmpleado'], $usuariosRegistran)) {
+?>
+    <li class="nav-item">
+    <a class="nav-link" href="index">
         <i class="fas fa-fw fa-home"></i>
         <span>Registrar Incidencias</span>
     </a>
 </li>
+<?php
+}
+?>
+
+
 <li class="nav-item">
-    <a class="nav-link" href="../inicio">
+    <a class="nav-link" href="seguimiento_incidencias">
         <i class="fas fa-fw fa-home"></i>
         <span>Seguimiento incidencias</span>
     </a>
