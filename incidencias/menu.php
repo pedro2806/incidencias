21a44af42eaf4f8fa9583147a2f2fb7aa9122304
Sebastian <sgutierrez26@alumnos.uaq.mx>
2026-02-26 15:56:34 -0600

    Se regreso la cookie de NoEmpleado
    Se regreso la cookie de NoEmpleado

--- incidencias/menu.php
+++ incidencias/menu.php
@@ -1,3 +1,9 @@
+<?php
+    include '../conn.php';
+    if($_COOKIE['noEmpleado'] == '' || $_COOKIE['noEmpleado'] == null){
+        echo '<script>window.location.assign("../index")</script>';
+    }
+?>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
 <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
